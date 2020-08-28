959420d850f1b33902625f358cd597bfd7f3dec6
Umit-Soylu <umit_soylu@yahoo.com>
2020-08-28 22:08:30 +0300

    Default constructor example added.

---
+++ src/com/bilgeadam/java/tutorials/constructors/DefaultConstructor.java
@@ -0,0 +1,10 @@
+package com.bilgeadam.java.tutorials.constructors;
+
+// Java will create a default no-arg constructor if not one defined by developer.
+public class DefaultConstructor {
+    String myName;
+
+    public String getMyName() {
+        return myName;
+    }
+}
