d88363c5a15246503e315354e9f6742de37b2a6f
Umit-Soylu <umit_soylu@yahoo.com>
2020-09-05 13:28:19 +0300

    Unnecessary Enumeration code removed

--- src/com/bilgeadam/java/tutorials/enumeration/Book.java
+++ src/com/bilgeadam/java/tutorials/enumeration/Book.java
@@ -11,12 +11,6 @@ public class Book {
     // Final variables should be initialized either in every constructor or in declaration
     private final String author; // Here author is initialized in every constructor.
 
-    // Reviewer of the book
-    private String reviewer;
-
-    // Number of pages.
-    private int nbOfPages;
-
     // State of the book
     private BookStages state; // Preparing, Reviewing, Published
 
@@ -60,40 +54,6 @@ public class Book {
             this.name = name;
         else
             throw new IllegalStateException("Book is already published");
-    }
-
-    /**
-     *
-     * @return name of the reviewer
-     */
-    public String getReviewer() {
-        return reviewer;
-    }
-
-    /**
-     * Set the reviewer according to criteria based on book type
-     * @param reviewer  Reviewer of the Book
-     */
-    protected void setReviewer(String reviewer) {
-        // TODO: Only allow reviewer based on book type.
-        this.reviewer = reviewer;
-    }
-
-    /**
-     *
-     * @return Number of pages
-     */
-    public int getNbOfPages() {
-        return nbOfPages;
-    }
-
-    /**
-     * Set number of pages according to criteria based on book type
-     * @param nbOfPages The number of pages
-     */
-    protected void setNbOfPages(int nbOfPages) {
-        // TODO: Set according to type.
-        this.nbOfPages = nbOfPages;
     }
 
     /**
